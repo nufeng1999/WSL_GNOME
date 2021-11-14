59483484251e80143fe4e66f7dea15eb086a8816
nufeng1999 <18478162@qq.com>
2021-11-15 07:27:45 +0800

    modify XWin startup mode

--- 00-wsl2-systemd.sh
+++ 00-wsl2-systemd.sh
@@ -1,3 +1,4 @@
+/mnt/c/WINDOWS/System32/cmd.exe /c "C:\\XWin_Cygwin.cmd 1" > /dev/null 2>&1 &
 SYSTEMD_PID=$(ps -ef | grep '/lib/systemd/systemd --system-unit=basic.target$' | grep -v unshare | awk '{print $2}')
 
 if [ -z "$SYSTEMD_PID" ]; then
