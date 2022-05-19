798245b222d3421a3404c9dbb55c6683a108d46a
nufeng1999 <18478162@qq.com>
2022-05-19 22:41:14 +0800

    Update 00-wsl2-systemd.sh
    add sleep

--- 00-wsl2-systemd.sh
+++ 00-wsl2-systemd.sh
@@ -5,8 +5,10 @@
 SYSTEMD_PID=$(ps -ef | grep '/lib/systemd/systemd --system-unit=basic.target$' | grep -v unshare | awk '{print $2}')
 if [ -z "$SYSTEMD_PID" ]; then
    sudo /usr/bin/daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
+   sleep 3
    SYSTEMD_PID=$(ps -ef | grep '/lib/systemd/systemd --system-unit=basic.target$' | grep -v unshare | awk '{print $2}')
 fi
 if [ -n "$SYSTEMD_PID" ] && [ "$SYSTEMD_PID" != "1" ]; then
     exec sudo /usr/bin/nsenter -t $SYSTEMD_PID -m -p su - $LOGNAME
+    sleep 3
 fi
