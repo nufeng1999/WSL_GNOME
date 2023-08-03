e7c97ef0499bef271034d04078afcd7f7949d2fe
nufeng1999 <nufeng1999@gmail.com>
2023-08-03 17:08:01 +0800

    Update startgnome2.sh

--- startgnome2.sh
+++ startgnome2.sh
@@ -11,5 +11,5 @@ export GDK_SCALE=1
 export GDK_DPI_SCALE=1
 #/usr/bin/gnome-session >/dev/null 2>&1 &
 cd $HOME
-sudo su $USER -c '/usr/bin/gnome-session' &
+sudo su $USER -c '/usr/bin/gnome-session'
 
