8e0770aa8b928cab2269c5acc72eb22f409b75b4
root <root@DESKTOP-VA0CQHI>
2024-01-08 12:45:22 +0800

    fix setproxy

--- setproxy.sh
+++ setproxy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 export HOSTIP=$(ip route | awk '/^default/{print $3; exit}')
 local_ip=`/sbin/ifconfig -a|grep eth0 -A 1| grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
-HOSTIPC=`/mnt/c/Windows/System32/ipconfig.exe|grep  "Default Gateway"|grep $HOSTIP|wc -l`
+HOSTIPC=`/mnt/c/Windows/System32/ipconfig.exe|iconv  -f gbk -t utf-8|grep $HOSTIP|wc -l`
 if [ $HOSTIPC -gt 0 ];then
     export HOSTIP=$local_ip
 fi
