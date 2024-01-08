c923ff22cdcfdc16061750ece52705cc7c0ac9b8
root <root@DESKTOP-VA0CQHI>
2024-01-08 13:04:56 +0800

    fix setguienv

--- setproxy.sh
+++ setproxy.sh
@@ -2,12 +2,14 @@
 export HOSTIP=$(ip route | awk '/^default/{print $3; exit}')
 local_ip=`/sbin/ifconfig -a|grep eth0 -A 1| grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "addr:"`
 HOSTIPC=`/mnt/c/Windows/System32/ipconfig.exe|iconv  -f gbk -t utf-8|grep $HOSTIP|wc -l`
+export HTTPPROXY=http://$HOSTIP:1080
 if [ $HOSTIPC -gt 0 ];then
     export HOSTIP=$local_ip
+    export HTTPPROXY=http://127.0.0.1:1080
 fi
-export DISPLAY=$HOSTIP:0.0
-export PULSE_SERVER=tcp:$HOSTIP
-export HTTPPROXY=http://$HOSTIP:1080
+#export DISPLAY=$HOSTIP:0.0
+#export PULSE_SERVER=tcp:$HOSTIP
+
 npm config set proxy $HTTPPROXY
 npm config set https-proxy $HTTPPROXY
 git config --global http.proxy $HTTPPROXY
