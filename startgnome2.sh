2f0701aafca3b1130e93088097748a2878b3ebb2
nufeng1999 <18478162@qq.com>
2024-01-14 23:42:57 +0800

    fix startgnome2

--- startgnome2.sh
+++ startgnome2.sh
@@ -3,8 +3,8 @@ cd /mnt/c
 #export WAYLAND_DISPLAY=wayland-0
 #export DISPLAY=:0
 #update-binfmts --disable cli
-/usr/bin/cygXS -c "$CYGWINDIR" -i "$ISWITCHDIR" -n9 > /dev/null 2>&1 &
-/usr/bin/cygXS -c "$CYGWINDIR" -i "$ISWITCHDIR" -n1 > /dev/null 2>&1 &
+/usr/bin/cygXS -c "$CYGWINDIR" -i "$ISWITCHDIR" -p2 -n9 > /dev/null 2>&1 &
+/usr/bin/cygXS -c "$CYGWINDIR" -i "$ISWITCHDIR" -p2 -n1 > /dev/null 2>&1 &
 sleep 5
 /usr/bin/fcitx -d -r >/dev/null 2>&1 &
 export GDK_SCALE=1.75
